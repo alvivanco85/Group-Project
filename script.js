5c8e21f16fccfad0807cc71a712ea6a5b4d503c2
Thanh Ngo <510thanh.ngo@gmail.com>
2020-02-13 04:01:27 -0800

    Adding button function & setting up functions

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
+$(document).on('keypress',function(e) {
+    if(e.which == 13) {
+      search();
+    }
+});
 $("#searchBtn").on("click", function () {
-    console.log("clicked");
-    $("#result-cont").removeClass("hide");
-    $("#songTitle").removeClass("hide");
-    $("#albumCover").removeClass("hide");
-    $("#lyrics").removeClass("hide");
+    search();
+    /*
+    var data = {
+        'url': 'https://api.audd.io/findLyrics/?q=Adele',
+        'return': 'timecode,spotify,lyrics',
+        'api_token': 'aac442290d3b6228e5387dc8352ecb4e'
+    };
+    $("#lyrics").text(data.result.lyrics.lyrics);
+    $(".title").text(data.result.title);
+    $(".artist").text(data.result.artist);
+    $(".album").text(data.result.album);
+
+
+        $("#result-cont").show();
+        $("#songTitle").show();
+        $("#albumCover").show();
+        $("#lyrics").show();
+    */
+
+
+
+
 });
 $("#logo").on("click", function () {
     console.log("logo click");
+
+    alert(searchInput);
 });
-var data = {
-    'url': 'https://audd.tech/example1.mp3',
-    'return': 'timecode,spotify,lyrics',
-    'api_token': 'aac442290d3b6228e5387dc8352ecb4e'
+
+function search() {
+  // SEARCH INPUT BY USER
+  var searchInput = $("#search-input").val();
+
+  // GET RESULTS WITH API
+  var queryURL = "https://api.audd.io/findLyrics/?q=" + searchInput + "&api_token=aac442290d3b6228e5387dc8352ecb4e";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(respond) {
+    console.log(respond);
+    var result = respond.data;
+  });
 }
-$.getJSON('https://api.audd.io/?jsonp=?', data, function (result) {
-    $("#lyrics").text(result.result.lyrics.lyrics);
-    console.log(result);
-});
